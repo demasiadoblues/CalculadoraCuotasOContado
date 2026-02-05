fc77112d3a0bd2bbb910a6c4b901f3b2b8324a52
sebastian <manatiqa@outlook.com>
2026-02-05 11:07:16 -0300

    Creando calculadora,primeros pasos
    cargando los php generales

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Simulador Financiero</title>
+  <link rel="stylesheet" href="css/estilos.css">
+  <script src="js/app.js" defer></script>
+</head>
+<body>
+  <header>
+    <h1>Simulador Financiero Comparativo</h1>
+  </header>
